deed2ffefbb028c49948b90c84bc5bd036666c59
JonasLuckeheide <jonaspuelo10@gmail.com>
2025-09-15 18:29:42 -0300

    Agrego cascaron de listado de encuestas pendientes

---
+++ frontend/react-app/src/hook/useEcuestas.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+enum EstadoEncuesta {
+    ABIERTA = "abierta",
+    CERRADA = "cerrada",
+}
+
+enum Cursado {
+    PrimerCuatrimestre = "cuatrimestre 1",
+    SegundoCuatrimestre = "cuatrimestre 2",
+    Anual = "Anual"
+}
+
+interface Encuesta {
+    id: number;
+    asignatura: string;
+    cursado: Cursado;
+    estado: EstadoEncuesta;  
+    fecha_fin: number;
+}
+
+export function useEncuestas(){
+
+    const[encuestas, setEncuestas] = useState<Encuesta[]>([]);
+    const[loading, setLoading] = useState<boolean>(true);
+    const[error, setError] = useState<string | null>(null);
+    
+    const API_URL = "http://localhost:8000/encuestas"; // Cambiá esto por la URL real de tu API
+
+    const fetchEncuestas = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(API_URL);
+            if (!response.ok) { 
+                throw new Error("Error al obtener las encuestas");
+            }
+            const data = await response.json();
+            setEncuestas(data);
+            setError(null);
+        }catch (err: any) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    }    
+
+
+useEffect(() => {
+    fetchEncuestas();
+}, []);
+
+return {
+    encuestas,
+    loading,
+    error,
+    refetch: fetchEncuestas 
+    };
+}
