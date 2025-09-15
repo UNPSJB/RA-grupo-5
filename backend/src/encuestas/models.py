3ae94c4bb0251869bb14f4316cf18ba234e1c730
Franco Martin Soler <FRANCOMARTIN2012@HOTMAIL.COM>
2025-09-15 18:08:54 -0300

    atributos encuesta arreglado

--- backend/src/encuestas/models.py
+++ backend/src/encuestas/models.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.models import ModeloBase
 from enum import Enum
 
+class Cursado(str, Enum):
+    cuatrimestre1 = "cuatrimestre 1"
+    cuatrimestre2 = "cuatrimestre 2"
+    anual= "anual"
 
 
 class EstadoEncuesta(str, Enum):
@@ -14,11 +18,13 @@ class Encuesta(ModeloBase):
     __tablename__ = "encuestas"
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    nombre: Mapped[str] = mapped_column(String, index=True)
     carrera: Mapped[str] = mapped_column(String, index=True)
-    actividad_curricular: Mapped[str] = mapped_column(String, index=True)
+    asignatura: Mapped[str] = mapped_column(String, index=True)
+    cursado: Mapped[Cursado] = mapped_column(SQLEnum(Cursado), nullable=False)
+    año: Mapped[int] = mapped_column(Integer, index=True)
     sede: Mapped[str] = mapped_column(String, index=True)    
     fecha_inicio: Mapped[str] = mapped_column(String, index=True)
     fecha_fin: Mapped[str] = mapped_column(String, index=True) 
     estado: Mapped[EstadoEncuesta] = mapped_column(SQLEnum(EstadoEncuesta), nullable=False, default=EstadoEncuesta.abierta)
+    
     
