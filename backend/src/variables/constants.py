976f2af11bc04d35506570e504762ffc25f1d322
DMLizama <lizama2696@gmail.com>
2025-09-18 12:47:25 -0300

    [fix] subiendo elementos faltantes de Variables

--- backend/src/variables/constants.py
+++ backend/src/variables/constants.py
@@ -1,3 +1,2 @@
 class ErrorCode:
     VARIABLE_NO_ENCONTRADA = "La variable no fue encontrada."
-    VARIABLE_ASOCIADA_A_ENCUESTA = "La variable se encuentra asociada a una encuesta"
--- backend/src/variables/exceptions.py
+++ backend/src/variables/exceptions.py
@@ -1,8 +1,5 @@
 from src.variables.constants import ErrorCode
 from src.exceptions import NotFound, BadRequest
 
-class VariableNoEncontrada(NotFound):
+class VariableEncontrada(NotFound):
     DETAIL = ErrorCode.VARIABLE_NO_ENCONTRADA
-
-class VariableAsociadaAEncuesta(BadRequest):
-    DETAIL = ErrorCode.VARIABLE_ASOCIADA_A_ENCUESTA
