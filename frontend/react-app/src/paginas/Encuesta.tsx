ec06819c8f0dc3365943764f9aca3358173b43e6
DMLizama <lizama2696@gmail.com>
2025-09-28 23:38:37 -0300

    "[Feature] completando hardcoding de variable en encuesta"

--- frontend/react-app/src/paginas/Encuesta.tsx
+++ frontend/react-app/src/paginas/Encuesta.tsx
@@ -72,7 +72,76 @@ export default function Encuesta() {
             <td>Escasos</td>
             <td>Suficientes</td>
           </tr>
-          <p>(*) Si eligión opción I (entre 0 y 50% de asistencia a clases):</p>
+          <p>
+            (*) Si eligión opción I (entre 0 y 50% de asistencia a
+            clases):¿Cuáles fueron los motivos por los cuales no asististe
+            regularmente a clases? Selecciona una de las siguientes opciones.
+            Indica con un cruz.
+          </p>
+        </tbody>
+      </Table>
+      <Table striped bordered hover>
+        <tbody>
+          <tr>
+            <td>a</td>
+            <td>Superposición de horarios con materias de otros años</td>
+            <td> </td>
+          </tr>
+          <tr>
+            <td>b</td>
+            <td>Incompatibilidad de horarios con tu trabajo.</td>
+            <td> </td>
+          </tr>
+          <tr>
+            <td>c</td>
+            <td>
+              Preferís formarte por t u cuenta con bibliografía y/o material de
+              trabajo
+            </td>
+            <td> </td>
+          </tr>
+          <tr>
+            <td>d</td>
+            <td>Enfermedad de larga duración</td>
+            <td> </td>
+          </tr>
+          <tr>
+            <td>e</td>
+            <td>Problemas personales</td>
+            <td> </td>
+          </tr>
+          <tr>
+            <td>f</td>
+            <td>Otros</td>
+            <td> </td>
+          </tr>
+        </tbody>
+      </Table>
+      <p>
+        A partir de aquí las personas responden teniendo en cuenta los
+        siguientes valores de referencia:
+      </p>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th></th>
+            <th> Valores de Referencia</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>Sí.</td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>No.</td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>NPO</td>
+            <td>No puedo opinar</td>
+          </tr>
         </tbody>
       </Table>
       {/* {encuesta.variables && encuesta.variables.length > 0 ? (
