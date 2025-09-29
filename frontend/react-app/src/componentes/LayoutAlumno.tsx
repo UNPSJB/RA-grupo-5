c586ecf7e407b1a2c7ef964272f163c5cfa65d9e
DMLizama <lizama2696@gmail.com>
2025-09-28 23:41:48 -0300

    "[Fix] corrigiendo algunos títulos y subtitlos"

--- frontend/react-app/src/componentes/LayoutAlumno.tsx
+++ frontend/react-app/src/componentes/LayoutAlumno.tsx
@@ -6,23 +6,28 @@ export default function LayoutAlumno() {
   return (
     <div className="layout">
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand className="p-4 text-white border-end" href="/">Encuestas UNPSJB</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/alumno/encuestas">Encuestas Pendientes</Nav.Link>
-            <Nav.Link as={Link} to="/alumno/mis-encuestas">Mis encuestas</Nav.Link>
-          </Nav>
+        <Navbar.Brand className="p-4 text-white border-end" href="/">
+          Encuestas UNPSJB
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/alumno/encuestas">
+            Encuestas Pendientes
+          </Nav.Link>
+          <Nav.Link as={Link} to="/alumno/mis-encuestas">
+            Mis encuestas
+          </Nav.Link>
+        </Nav>
       </Navbar>
 
-      
       <main className="content">
         {/* Contenido principal */}
         <Container className="mt-4 text-center">
-          <Outlet />  {/* Outlet solo renderiza páginas hijas, si las hubiera */}
+          <Outlet /> {/* Outlet solo renderiza páginas hijas, si las hubiera */}
         </Container>
       </main>
 
       <footer className="footer bg-dark text-white text-center p-3 mb-0">
-        &copy; 2025 Mi Sitio
+        &copy; 2025 Reportes de Alumnos
       </footer>
     </div>
   );
--- frontend/react-app/src/componentes/LayoutHome.tsx
+++ frontend/react-app/src/componentes/LayoutHome.tsx
@@ -6,23 +6,33 @@ export default function LayoutHome() {
   return (
     <div className="layout">
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand className="p-4 text-white border-end" href="/">Encuestas UNPSJB</Navbar.Brand>
+        <Navbar.Brand className="p-4 text-white border-end" href="/">
+          Encuestas UNPSJB
+        </Navbar.Brand>
       </Navbar>
 
-      
       <main className="content flex-grow-1">
-        <Container className="mt-4 d-flex flex-column align-items-center justify-content-end" style={{minHeight: '35vh'}}>
-            <Outlet />
-            <div className="d-flex gap-3 mt-5">
-            <Link to="/alumno" className="btn btn-dark  ">Rol Alumno</Link>
-            <Link to="/docente" className="btn btn-dark">Rol Docente</Link>
-            <Link to="/departamento" className="btn btn-dark">Rol Departamento</Link>
-            </div>
+        <Container
+          className="mt-4 d-flex flex-column align-items-center justify-content-end"
+          style={{ minHeight: "35vh" }}
+        >
+          <Outlet />
+          <div className="d-flex gap-3 mt-5">
+            <Link to="/alumno" className="btn btn-dark  ">
+              Rol Alumno
+            </Link>
+            <Link to="/docente" className="btn btn-dark">
+              Rol Docente
+            </Link>
+            <Link to="/departamento" className="btn btn-dark">
+              Rol Departamento
+            </Link>
+          </div>
         </Container>
       </main>
 
       <footer className="footer bg-dark text-white text-center p-3 mb-0">
-        &copy; 2025 Mi Sitio
+        &copy; 2025 Reportes de Alumnos
       </footer>
     </div>
   );
