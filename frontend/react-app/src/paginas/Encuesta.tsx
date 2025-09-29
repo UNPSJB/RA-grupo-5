f89e9939c0a7c2b055aa8c6a9ad458ac9f257b42
DMLizama <lizama2696@gmail.com>
2025-09-28 23:28:51 -0300

    "[feature] Hardcoding variables on Encuestas"

--- frontend/react-app/src/paginas/Encuesta.tsx
+++ frontend/react-app/src/paginas/Encuesta.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useEncuestas } from "../hook/useEncuestas";
+import Table from "react-bootstrap/Table";
 //import Variable from "../componentes/Variable";
 
 export default function Encuesta() {
@@ -20,11 +21,61 @@ export default function Encuesta() {
     <div>
       <h1>{encuesta.asignatura}</h1>
       <p>Estado: {encuesta.estado}</p>
-      <p><strong>Carrera:</strong> {encuesta.carrera}</p>
-      <p><strong>Sede:</strong> {encuesta.sede}</p>
+      <p>
+        <strong>Carrera:</strong> {encuesta.carrera}
+      </p>
+      <p>
+        <strong>Sede:</strong> {encuesta.sede}
+      </p>
 
       <h2>Variables asociadas</h2>
-      {encuesta.variables && encuesta.variables.length > 0 ? (
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>A:</th>
+            <th>Información general</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>A1</td>
+            <td>
+              ¿Cuántas veces te has inscripto para cursar esta asignatura?
+            </td>
+            <td>Una</td>
+            <td>Más de una</td>
+          </tr>
+          <tr>
+            <td>A2</td>
+            <td>
+              ¿Cuál ha sido aproximadamente tu porcentaje de asistencia a clases
+              teóricas?
+            </td>
+            <td>Entre 0 y 50%</td>
+            <td>Más de 50%</td>
+          </tr>
+          <tr>
+            <td>A3</td>
+            <td>
+              ¿Cuál ha sido aproximadamente tu porcentaje de asistencia a clases
+              prácticas?
+            </td>
+            <td>Entre 0 y 50%</td>
+            <td>Más de 50%</td>
+          </tr>
+          <tr>
+            <td>A4</td>
+            <td>
+              Los conocimientos previos para comprender los contenidos de la
+              asignatura fueron
+            </td>
+            <td>Escasos</td>
+            <td>Suficientes</td>
+          </tr>
+          <p>(*) Si eligión opción I (entre 0 y 50% de asistencia a clases):</p>
+        </tbody>
+      </Table>
+      {/* {encuesta.variables && encuesta.variables.length > 0 ? (
         <ul>
           {encuesta.variables.map((v: any) => (
             <li key={v.id}>{v.nombre}</li>
@@ -32,7 +83,7 @@ export default function Encuesta() {
         </ul>
       ) : (
         <p>No hay variables asociadas.</p>
-      )}
+      )} */}
     </div>
   );
 }
