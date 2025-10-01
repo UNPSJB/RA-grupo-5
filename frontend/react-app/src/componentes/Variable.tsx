8913f088c8d624e72f5a9728a6e1f1ad0bf7e1d7
DMLizama <lizama2696@gmail.com>
2025-10-01 16:04:45 -0300

    [fix] corrigiendo vista de la variable

--- frontend/react-app/src/componentes/Variable.tsx
+++ frontend/react-app/src/componentes/Variable.tsx
@@ -4,7 +4,9 @@ import type { VariableProps } from "../types/Variable";
 export default function Variable({ variable }: VariableProps) {
   return (
     <div className="container mt-4">
-      <h3>{variable.nombre}</h3>
+      <h3>
+        {variable.codigo}: {variable.nombre}
+      </h3>
 
       <Table striped bordered>
         <thead>
