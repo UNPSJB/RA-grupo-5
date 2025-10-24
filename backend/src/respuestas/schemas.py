9ce0767bdcb62ed325134dc97bcdb5c0a1b82b82
JonasLuckeheide <jonaspuelo10@gmail.com>
2025-10-23 21:02:42 -0300

    Ahora se guardan las respuestas del informe y de la encuesta con sus validaciones

--- backend/src/respuestas/schemas.py
+++ backend/src/respuestas/schemas.py
@@ -1,13 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from typing import Optional, List
 from src.detalle_respuesta.schemas import DetalleRespuestaCreate, DetalleRespuestaRead
 
 class RespuestaBase(BaseModel):
     id_persona: int
-    id_encuesta_asignatura: int
-
+    id_encuesta_asignatura: Optional[int] = None
+    id_informe_asignatura: Optional[int] = None
 class RespuestaCreate(RespuestaBase):
     detalles: List[DetalleRespuestaCreate]
+    
+    @model_validator(mode='after')
+    def check_exactly_one_context(self):
+        if self.id_encuesta_asignatura and self.id_informe_asignatura:
+            raise ValueError("La respuesta no puede pertenecer a una encuesta y un informe a la vez.")
+        if not self.id_encuesta_asignatura and not self.id_informe_asignatura:
+            raise ValueError("La respuesta debe pertenecer a una encuesta o a un informe.")
+        return self
 
 class RespuestaRead(RespuestaBase):
     id: int
--- backend/src/respuestas/services.py
+++ backend/src/respuestas/services.py
@@ -3,29 +3,42 @@ from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session, joinedload
 from src.respuestas.models import Respuesta
-from src.respuestas import schemas, exception
+from src.respuestas import schemas
 from src.pregunta_opcion.models import PreguntaOpcion
 from src.preguntas.models import Pregunta
 from src.detalle_respuesta.models import DetalleRespuesta
 
 
 def crear_respuesta(db: Session, respuesta: schemas.RespuestaCreate) -> Respuesta:
+    
+    es_informe = respuesta.id_informe_asignatura is not None
+    
     po_ids = [detalle.id_pregunta_opcion for detalle in respuesta.detalles]
     
     query = (
-        select(PreguntaOpcion.id,Pregunta.tipo,Pregunta.obligatoria)
+        select(
+            PreguntaOpcion.id,
+            Pregunta.tipo,
+            Pregunta.obligatoria,
+            Pregunta.id_informe_base,
+            )
         .join(Pregunta,PreguntaOpcion.id_pregunta == Pregunta.id)
         .where(PreguntaOpcion.id.in_(po_ids))
     )
 
     pregunta_info = {
-        po_id: {"tipo": tipo, "obligatoria": obligatoria}
-        for po_id, tipo, obligatoria in db.execute(query).all()
+        po_id: {
+            "tipo": tipo, 
+            "obligatoria": obligatoria,
+            "es_de_informe": id_informe_base is not None 
+        }
+        for po_id, tipo, obligatoria, id_informe_base in db.execute(query).all()
     }
 
     db_respuesta = Respuesta(
         id_persona=respuesta.id_persona,
-        id_encuesta_asignatura=respuesta.id_encuesta_asignatura
+        id_encuesta_asignatura=respuesta.id_encuesta_asignatura,
+        id_informe_asignatura=respuesta.id_informe_asignatura
     )
     db.add(db_respuesta)
     db.flush()
@@ -44,21 +57,22 @@ def crear_respuesta(db: Session, respuesta: schemas.RespuestaCreate) -> Respuest
             )
         tipo_pregunta = info["tipo"]
         es_obligatoria = info["obligatoria"]
+        
+        
 
-        if tipo_pregunta != "open" and texto_recibido is not None:
+        if tipo_pregunta != "open" and texto_recibido is not None and texto_recibido.strip() != "":
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"No se permite texto libre para la pregunta {po_id}, no es de tipo abierto"
             )
-
-        if(
+        if (
             tipo_pregunta == "open" and
             es_obligatoria and
             (texto_recibido is None or texto_recibido.strip() == "")
         ):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"El texto es obligatoria para la pregunta {po_id}, ya que es de tipo abierta"   
+                detail=f"El texto es obligatoria para la pregunta {po_id}"
             )
         db_detalle = DetalleRespuesta(
             **detalle_schema.model_dump(),
@@ -76,22 +90,24 @@ def crear_respuesta(db: Session, respuesta: schemas.RespuestaCreate) -> Respuest
 
 def listar_respuestas(db: Session,
     persona_id: Optional[int] = None,
-    encuesta_asignatura_id: Optional[int] = None
- ) -> List[Respuesta]:
+    encuesta_asignatura_id: Optional[int] = None,
+    informe_asignatura_id: Optional[int] = None
+) -> List[Respuesta]:
     query = (
-         select(Respuesta)
-         .options(joinedload(Respuesta.detalles))
+        select(Respuesta)
+        .options(joinedload(Respuesta.detalles))
     )
 
     if persona_id is not None:
-        query = query.where(Respuesta.id_persona == persona_id)    
+        query = query.where(Respuesta.id_persona == persona_id)
 
     if encuesta_asignatura_id is not None:
-        query = query.where(Respuesta.id_encuesta_asignatura == encuesta_asignatura_id)    
-
-    return db.scalars(query).all()
-
-
+        query = query.where(Respuesta.id_encuesta_asignatura == encuesta_asignatura_id)
+    
+    if informe_asignatura_id is not None:
+        query = query.where(Respuesta.id_informe_asignatura == informe_asignatura_id)
+        
+    return db.scalars(query).unique().all()
 
 def leer_respuesta(db: Session, respuesta_id: int) -> Respuesta:
     
