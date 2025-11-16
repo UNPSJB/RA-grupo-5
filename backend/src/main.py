d402bf05fdc5638302e2c2b6fe699be6133f6be3
DMLizama <lizama2696@gmail.com>
2025-11-16 19:08:48 -0300

    [feature] agregando login JWT y get_current_persona con token

--- backend/src/main.py
+++ backend/src/main.py
@@ -23,6 +23,7 @@ from src.informes_sinteticos_base.router import router as informes_sinteticos_ba
 from src.carreras.router import router as carreras_router
 from src.informe_sintetico_carrera.router import router as informe_sintetico_carrera_router
 from src.seguridad.router import router as seguridad_router
+from src.auth.router import router as auth_router
 
 from src.seguridad.services import SeguridadService
 
@@ -93,3 +94,4 @@ app.include_router(informes_sinteticos_base_router)
 app.include_router(carreras_router)
 app.include_router(informe_sintetico_carrera_router)
 app.include_router(seguridad_router)
+app.include_router(auth_router)
